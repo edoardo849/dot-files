4bcf75f686127fa2da6fa6d02bb5678900413102
Edoardo Paolo Scalafiotti <edoardo849@gmail.com>
2016-10-26 15:36:23 +0100

    New files

---
+++ bashrc.sh
@@ -0,0 +1,11 @@
+
+# GoLang
+export GOPATH=$HOME/go
+export TERRAFORM=$HOME/terraform
+
+# Rust
+export PATH="~/.cargo/bin:$PATH"
+export RUST_SRC_PATH="~/src/rust/src"
+
+export PATH=$GOPATH/bin:/usr/local/terraform/bin:$TERRAFORM:$PATH
+
