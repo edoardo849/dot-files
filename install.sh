03a646c7550db2d201efc24e2a29ec8304c21a9e
Edoardo Paolo Scalafiotti <edoardo849@gmail.com>
2016-11-09 09:35:00 +0000

    New Install script

--- install.sh
+++ install.sh
@@ -1,182 +1,253 @@
 
-cd $HOME
-
-dotFiles=$HOME/.dotfiles
-if [ ! -d "$dotFiles" ]; then
-	git clone git@github.com:edoardo849/dot-files.git $dotFiles
-fi
 
 
-#pacmanPkgs = ()
 
-supported_pkg () {
-	echo 'Installing supported packages'
+function ask_yes_or_no() {
+	read -p "$1 ([y]es or [N]o): "
+	case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+		y|yes) echo "yes" ;;
+		*)     echo "no" ;;
+	esac
+}
 
-	sudo pacman -S \
-		xf86-input-libinput \
-		gnupg \
-		redshift \
-		ruby \
-		zsh \
-		zsh-completions \
-		nodejs \
-		npm \
-		neovim \
-		python2-neovim \
-		python-neovim \
-		jq \
-		go \
-		clang # for swift
+install_pacman () {
+	echo 'Installing Pacman packages'
+	installPkgs=('gnupg' 'xf86-input-libinput' 'redshift' 'ruby' 'zsh' 'zsh-completions' 'nodejs' 'npm'  'neovim' 'python2-neovim' 'python-neovim' 'jq' 'go' 'clang' )
+
+	for i in "${installPkgs[@]}"
+	do
+		if [[ "no" == $(ask_yes_or_no "Install $i ?")  ]]
+		then
+			echo "$i skipped."
+			continue
+		else
+			echo "Installing $i"
+			sudo pacman -S $i
+		fi
+
+	done
 
 }
 
-community_pkg () {
 
-	echo 'Installing community packages'
+install_yaourt () {
+	echo 'Installing AUR packages'
+	installPkgs=('touchegg' 'google-chrome' 'spotify' 'insync' 'swift')
 
-	yaourt \
-		touchegg \
-		google-chrome \
-		spotify \
-		insync \
-		swift
+	for i in "${installPkgs[@]}"
+	do
+		if [[ "no" == $(ask_yes_or_no "Install $i ?")  ]]
+		then
+			echo "$i skipped."
+			continue
+		else
+			echo "Installing $i"
+			yaourt $i
+		fi
+
+	done
 }
 
-# Install Gnupg
-#if hash gpg 2>/dev/null; then
-#	echo 'Gnupg already installed'
-#else
-#	echo 'Installing Gnupg'
-#	sudo pacman -S gnupg
-#fi
 
-#if hash /usr/local/go/bin/go 2>/dev/null; then
-#	echo 'GoLang already installed'
-#else
-#	echo 'Installing GoLang 1.7'
-#	wget
-#"https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz" -P /tmp && sudo tar -C /usr/local -xzf
-#/tmp/go1.7.3.linux-amd64.tar.gz
-#fi
+install_rust () {
+	if [[ "no" == $(ask_yes_or_no "Install Rust ?")  ]]
+	then
+		echo "Skipped."
+		return 1
+	else
+		echo "Installing Rust"
+		curl -sSf https://static.rust-lang.org/rustup.sh | sh
+		cargo install racer
+	fi
+}
 
 
-#echo 'Setup Neovim and Plug'
-#curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-	#
-#https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+setup_neovim_plug () {
+	if [[ "no" == $(ask_yes_or_no "Install Plug for NeoVim ?")  ]]
+	then
+		echo "Skipped."
+		return 1
+	else
+		echo "Installing Plug for NeoVIm"
 
-#echo 'Installling Rust'
-#curl -sSf https://static.rust-lang.org/rustup.sh | sh
-# Code completion for rust
-#cargo install racer
+		curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	fi
 
-echo 'Setting up Development environment'
-devDir=$HOME/Development
-if [ ! -d "$devDir" ]; then
-	mkdir $devDir
-fi
+}
 
-#if hash terraform 2>/dev/null; then
-#	echo 'Terraform already installed'
-#else
-#	echo 'Installing Terraform'
-#	terraform_url=$(curl --silent
-#https://releases.hashicorp.com/index.json | jq '{terraform}' | egrep "linux.*64" | sort -rh | head -1 | awk -F[\"] '{print $4}')
-#	terDir=$devDir/terraform
-#	mkdir $terDir
-#	curl -o $terDir/terraform.zip $terraform_url
-#	cd $terDir
-#	unzip terraform.zip
-#	rm terraform.zip
-#	cd $HOME
-#fi
+terraform () {
 
-#echo 'Installing Amazon AWS CLI'
-#curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o
-#"/tmp/awscli-bundle.zip"
-#cd /tmp
-#unzip awscli-bundle.zip
-#sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
-#cd $HOME
+	if [[ "no" == $(ask_yes_or_no "Install Terraform ?")  ]]
+	then
+		echo "Skipped."
+		return 1
+	else
+		echo "Installing Terraform"
+		if hash terraform 2>/dev/null; then
+			echo 'Terraform already installed'
+		else
+			echo 'Installing Terraform'
+			terraform_url=$(curl --silent
+			https://releases.hashicorp.com/index.json | jq '{terraform}' | egrep "linux.*64" | sort -rh | head -1 | awk -F[\"] '{print $4}')
+			terDir=$devDir/terraform
+			mkdir $terDir
+			curl -o $terDir/terraform.zip $terraform_url
+			cd $terDir
+			unzip terraform.zip
+			rm terraform.zip
+			cd $HOME
+		fi
 
-goDir=$devDir/go
-if [ ! -d "$goDir" ]; then
-	echo "- Creating $goDir"
-	mkdir $goDir
-fi
+	fi
+}
 
-vimFile=$HOME/.vimrc
-if [ -f "$vimFile" ] && [ ! -L "$vimFile" ]; then
-	echo "- Backing up $vimFile"
-	mv $vimFile $vimFile.backup
-fi
+global_npm () {
+	echo 'Installing NPM global packages'
+	installPkgs=('js-beautify')
 
-if [ ! -L "$vimFile" ]; then
-	echo "- Linking $vimFile to $dotFiles"
-	ln -s $dotFiles/init.vim $vimFile
-fi
+	for i in "${installPkgs[@]}"
+	do
+		if [[ "no" == $(ask_yes_or_no "Install $i ?")  ]]
+		then
+			echo "$i skipped."
+			continue
+		else
+			echo "Installing $i"
+			sudo npm install -g $i
+		fi
 
-redshiftConf=$HOME/.config/redshift.conf
-if [ -f "$redshiftConf" ] && [ ! -L "$redshiftConf" ]; then
-	echo "- Backing up $redshiftConf"
-	mv $redshiftConf $redshiftConf.backup
-fi
+	done
 
-if [ ! -L "$redshiftConf" ]; then
-	echo "- Linking $redshiftConf to $dotFiles"
-	ln -s $dotFiles/redshift.conf $redshiftConf
-fi
-
-vimConfig=$HOME/.config/nvim/init.vim
-if [ -f "$vimConfig" ] && [ ! -L "$vimConfig" ]; then
-	echo "- Backing up $vimConfig"
-	mv $vimConfig $vimConfig.backup
-fi
-
-if [ ! -L "$vimConfig" ]; then
-	echo "- Linking $vimConfig to $dotFiles"
-	ln -s $dotFiles/init.vim $vimConfig
-fi
-
-vimColorFolder=$HOME/.vim/colors
-
-if [ ! -L "$vimColorFolder" ]; then
-	echo "- Backing up $vimColorFolder"
-	ln -s $dotFiles/vim/colors $vimColorFolder
-fi
-
-# GIT Config
-gitConfig=$HOME/.gitconfig
-if [ -f "$gitConfig" ] && [ ! -L "$gitConfig" ]; then
-	echo "- Backing up $gitConfig"
-	mv $gitConfig $gitConfig.backup
-fi
-
-if [ ! -L "$gitConfig" ]; then
-	echo "- Likning $gitConfig to $dotFiles"
-	ln -s $dotFiles/gitconfig $gitConfig
-fi
-
-# ZSH Config
-zshConfig=$HOME/.zshrc
-if [ -f "$zshConfig" ] && [ ! -L "$zshConfig" ]; then
-	echo "- Backing up $zshConfig"
-	mv $zshConfig $zshConfig.backup
-fi
+}
 
 
-if [ ! -f "$zshConfig" ] && [ ! -L "$zshConfig" ]; then
-	echo "- Setting up Zsh"
-	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-fi
+install_amazonawscli () {
+	if [[ "no" == $(ask_yes_or_no "Install AWS CLI ?")  ]]
+	then
+		echo "Skipped."
+		continue
+	else
+		echo "Installing AWS CLI"
+		curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/tmp/awscli-bundle.zip"
+		cd /tmp
+		unzip awscli-bundle.zip
+		sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+		cd $HOME
+	fi
 
 
-if [ ! -L "$zshConfig" ]; then
-	echo "- Linking $zshConfig to $dotFiles"
-	ln -s $dotFiles/zshrc.sh $zshConfig
-fi
+}
 
-echo 'Installing js-beautify'
-sudo npm install -g js-beautify
+link_dotfiles () {
+	if [[ "no" == $(ask_yes_or_no "Link config to dotfiles ?")  ]]
+	then
+		echo "Skipped."
+		return 1
+	else
+		echo "Linking..."
 
-echo 'Success! Now run "chsh -s /bin/zsh" to setup zsh as the default shell and run :PlugInstall from within nvim'
+		echo 'Setting up Development environment'
+		devDir=$HOME/Development
+		if [ ! -d "$devDir" ]; then
+			mkdir $devDir
+		fi
+
+		goDir=$devDir/go
+		if [ ! -d "$goDir" ]; then
+			echo "- Creating $goDir"
+			mkdir $goDir
+		fi
+
+		vimFile=$HOME/.vimrc
+		if [ -f "$vimFile" ] && [ ! -L "$vimFile" ]; then
+			echo "- Backing up $vimFile"
+			mv $vimFile $vimFile.backup
+		fi
+
+		if [ ! -L "$vimFile" ]; then
+			echo "- Linking $vimFile to $dotFiles"
+			ln -s $dotFiles/init.vim $vimFile
+		fi
+
+		redshiftConf=$HOME/.config/redshift.conf
+		if [ -f "$redshiftConf" ] && [ ! -L "$redshiftConf" ]; then
+			echo "- Backing up $redshiftConf"
+			mv $redshiftConf $redshiftConf.backup
+		fi
+
+		if [ ! -L "$redshiftConf" ]; then
+			echo "- Linking $redshiftConf to $dotFiles"
+			ln -s $dotFiles/redshift.conf $redshiftConf
+		fi
+
+		vimConfig=$HOME/.config/nvim/init.vim
+		if [ -f "$vimConfig" ] && [ ! -L "$vimConfig" ]; then
+			echo "- Backing up $vimConfig"
+			mv $vimConfig $vimConfig.backup
+		fi
+
+		if [ ! -L "$vimConfig" ]; then
+			echo "- Linking $vimConfig to $dotFiles"
+			ln -s $dotFiles/init.vim $vimConfig
+		fi
+
+		vimColorFolder=$HOME/.vim/colors
+
+		if [ ! -L "$vimColorFolder" ]; then
+			echo "- Backing up $vimColorFolder"
+			ln -s $dotFiles/vim/colors $vimColorFolder
+		fi
+
+		# GIT Config
+		gitConfig=$HOME/.gitconfig
+		if [ -f "$gitConfig" ] && [ ! -L "$gitConfig" ]; then
+			echo "- Backing up $gitConfig"
+			mv $gitConfig $gitConfig.backup
+		fi
+
+		if [ ! -L "$gitConfig" ]; then
+			echo "- Likning $gitConfig to $dotFiles"
+			ln -s $dotFiles/gitconfig $gitConfig
+		fi
+
+		# ZSH Config
+		zshConfig=$HOME/.zshrc
+		if [ -f "$zshConfig" ] && [ ! -L "$zshConfig" ]; then
+			echo "- Backing up $zshConfig"
+			mv $zshConfig $zshConfig.backup
+		fi
+
+
+		if [ ! -f "$zshConfig" ] && [ ! -L "$zshConfig" ]; then
+			echo "- Setting up Zsh"
+			sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+		fi
+
+
+		if [ ! -L "$zshConfig" ]; then
+			echo "- Linking $zshConfig to $dotFiles"
+			ln -s $dotFiles/zshrc.sh $zshConfig
+		fi
+
+		echo 'Success! Now run "chsh -s /bin/zsh" to setup zsh as the default shell and run :PlugInstall from within nvim'
+	fi
+}
+
+
+
+install_pacman
+
+install_yaourt
+
+install_rust
+
+setup_neovim_plug
+
+terraform
+
+global_npm
+
+install_amazonawscli
+
+link_dotfiles
